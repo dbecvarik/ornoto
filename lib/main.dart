76ab1f7c9f0c3b507e8f3a1fc339cbccb56e3487
David Becvarik <david@becvarik.cz>
2020-10-31 21:28:52 +0100

    dynamically adding widgets

--- lib/main.dart
+++ lib/main.dart
@@ -28,72 +28,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> widgets = new List();
+  var center = TextAlign.center;
+  var _textStyle = TextStyle(fontSize: 36.0);
+
+  RawKeyboardListener getTextWidget() {
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      onKey: (event) => handleKey(event),
+      child: TextField(
+        maxLines: null,
+        minLines: 1,
+        textAlign: center,
+        style: _textStyle,
+        onChanged: (text) {
+          RegExp regexp = new RegExp(
+            r".*^/$",
+          );
+          if (regexp.hasMatch(text)) {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => widgetDialog());
+          }
+        },
+      ),
+    );
+  }
+
+  getWidgets() {
+    if (widgets.isEmpty) {
+      widgets.add(getTextWidget());
+    }
+    return widgets;
+  }
+
+  addWidget() {
+    print("Adding widget");
+    widgets.add(getTextWidget());
+    setState(() {});
+  }
+
+  handleKey(RawKeyEvent event) {
+    if (event.isKeyPressed(LogicalKeyboardKey.enter) && event.isShiftPressed) {
+      print("DEBUG: Shift Enter");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    var center = TextAlign.center;
-    var _textStyle = TextStyle(fontSize: 36.0);
-
-    handleKey(RawKeyEvent event) {
-      if (event.isKeyPressed(LogicalKeyboardKey.enter) &&
-          event.isShiftPressed) {
-        print("DEBUG: Shift Enter");
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: getWidgets()),
+      ),
+    );
+  }
+
+  Dialog widgetDialog() {
+    return new Dialog(
+      child: Container(
+        height: 300.0,
+        width: 360.0,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            RawKeyboardListener(
-              focusNode: FocusNode(),
-              onKey: (event) => handleKey(event),
-              child: TextField(
-                maxLines: null,
-                minLines: 1,
-                textAlign: center,
-                style: _textStyle,
-                onChanged: (text) {
-                  RegExp regexp = new RegExp(
-                    r".*^/$",
-                  );
-                  if (regexp.hasMatch(text)) {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) => widgetDialog);
-                  }
-                },
-              ),
+            Text(
+              'Choose from Library',
+              style: TextStyle(color: Colors.black, fontSize: 22.0),
             ),
+            FlatButton(
+              onPressed: () {
+                addWidget();
+              },
+              child: new Text('Textbox'),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-Dialog widgetDialog = Dialog(
-  child: Container(
-    height: 300.0,
-    width: 360.0,
-    color: Colors.white,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        Text(
-          'Choose from Library',
-          style: TextStyle(color: Colors.black, fontSize: 22.0),
-        ),
-        FlatButton(
-          onPressed: () {
-            print("pressed");
-          },
-          child: new Text('Textbox'),
-        )
-      ],
-    ),
-  ),
-);
